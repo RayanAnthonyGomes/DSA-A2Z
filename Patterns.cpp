27bf4dc76f71cc902a087f34a833046505feaf19
RayanGomes <magirayan84@gmail.com>
2025-03-20 12:00:42 +0600

    Did Pattern 4

--- Patterns.cpp
+++ Patterns.cpp
@@ -85,9 +85,34 @@ void pattern3(int n){
     }
 }
     
+/*  Pattern 4
+Input Format: N = 3
+Result: 
+1
+2 2 
+3 3 3
 
+Input Format: N = 6
+Result:
+1
+2 2
+3 3 3
+4 4 4 4
+5 5 5 5 5
+6 6 6 6 6 6
+*/
 
-
+void pattern4(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i;
+        }
+        cout << "\n";
+    }
+    
+}
 
 
 
@@ -111,6 +136,6 @@ int main(){
     */
     int n;
     cin >> n;
-    pattern3(n);
+    pattern4(n);
 
 }
