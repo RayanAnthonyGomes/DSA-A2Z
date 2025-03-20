a6c3a60eea31eb5ad64ad22e67cf283a2e79b875
RayanGomes <magirayan84@gmail.com>
2025-03-20 12:58:27 +0600

    Did Pattern 7

--- Patterns.cpp
+++ Patterns.cpp
@@ -173,6 +173,48 @@ void pattern6(int n){
     
 }
 
+/* Pattern 7
+Input Format: N = 3
+Result: 
+  *  
+ *** 
+*****   
+Input Format: N = 6
+Result:
+     *     
+    ***    
+   *****   
+  *******  
+ ********* 
+***********
+As we can clearly observe that for each row there are some spaces that get printed then some stars and then again some spaces giving it a final pyramidal look. For eg: In the first row (i=0) there are 4 spaces, 1 star, then again 4 spaces. In the second row (i=1) there are 3 spaces, 3 stars, then again 3 spaces so we can say that there are N-i-1 spaces, 2*i+1 stars, and then again N-i-1 spaces for each row where i is the row index. We thus simply run 3 inner loops first for printing the spaces, then the stars, and then the spaces again.
+*/
+
+void pattern7(int n){
+    for (int i = 0; i < n; i++)
+    {
+        //spaces
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout << " ";
+        }
+        //stars
+        for (int j = 0; j < 2*i+1; j++)
+        {
+            cout << "*";
+
+        }
+        //spaces
+        for (int j = 0; j < n-i-1; j++)
+        {
+            cout << " ";
+        }
+        cout << "\n";
+        
+    }
+    
+}
+
 
 
 
@@ -190,6 +232,6 @@ int main(){
     */
     int n;
     cin >> n;
-    pattern6(n);
+    pattern7(n);
 
 }
