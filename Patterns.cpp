ed8ff15e8b98a3d94de36d8cef076a7f15fb8920
RayanGomes <magirayan84@gmail.com>
2025-03-20 11:48:15 +0600

    Did pattern 1

--- Patterns.cpp
+++ Patterns.cpp
@@ -1,9 +1,59 @@
 #include<iostream>
 using namespace std;
+/*  Pattern 1
+Example 1:
+Input: N = 3
+Output: 
+* * *
+* * *
+* * *
+Example 2:
+Input: N = 6
+Output:
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+* * * * * *
+*/
+void pattern1(int n){
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "* ";
+        }
+        cout << "\n";
+    }
+    
+}
+
+/*  Pattern 2
+Input Format: N = 3
+Result: 
+* 
+* * 
+* * *
+
+Input Format: N = 6
+Result:
+* 
+* * 
+* * *
+* * * *
+* * * * *
+* * * * * *
+*/
+
+void pattern2(int n){
+
+}
+
 
 int main(){
     int n;
     cin >> n;
-    cout << n;
-    
+    pattern1(n);
+
 }
