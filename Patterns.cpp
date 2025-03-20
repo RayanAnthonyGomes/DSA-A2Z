4f0f44db908e48739da34aaec95606d7b9af98b6
RayanGomes <magirayan84@gmail.com>
2025-03-20 12:21:59 +0600

    Did pattern 6

--- Patterns.cpp
+++ Patterns.cpp
@@ -145,7 +145,33 @@ void pattern5(int n){
     
 }
 
+/*  Pattern 6
+Input Format: N = 3
+Result: 
+1 2 3
+1 2
+1
 
+Input Format: N = 6
+Result:
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2 
+1
+*/
+void pattern6(int n){
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j < n + 2 - i; j++)
+        {
+            cout << j;
+        }
+        cout << "\n";
+    }
+    
+}
 
 
 
@@ -164,6 +190,6 @@ int main(){
     */
     int n;
     cin >> n;
-    pattern5(n);
+    pattern6(n);
 
 }
