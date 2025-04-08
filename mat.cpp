6e526f25e10a624d5fc01f5edb765a0f5e518dac
RayanGomes <magirayan84@gmail.com>
2025-04-08 15:05:02 +0600

    armstrong numbers

--- mat.cpp
+++ mat.cpp
@@ -53,7 +53,7 @@ void Palindrome(int n){
         revN = (revN*10) + lastDigit;
         n = n/10;
     }
-    if(revN=duplicate){
+    if(revN==duplicate){
         return true;
     }else{
         return false;
@@ -61,6 +61,20 @@ void Palindrome(int n){
     
 }
 
+void ArmStrongNumbers(int n){
+    //n = 371 == 3^3 + 7^3 + 1^3
+    int sum = 0,dup = n;
+    while (n>0)
+    {
+        int ld = n%10;
+        sum = sum + (ld)*(ld)*(ld);
+        n = n/10;
+
+    }
+    if( dup == sum ){
+        cout >> "Armstrong";
+    }
+}
 
 int main(){
     int n;
