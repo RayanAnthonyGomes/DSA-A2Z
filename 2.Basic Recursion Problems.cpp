43468ae9bbcd06f6921284314275e10f431f6371
RayanGomes <magirayan84@gmail.com>
2025-04-30 13:58:41 +0600

    Update Basic PRobs

--- 2.Basic Recursion Problems.cpp
+++ 2.Basic Recursion Problems.cpp
@@ -8,9 +8,22 @@ void f(int i,int n){
     f(i+1,n);
 }
 
+//Print Linearly from 1 to N
+void f2(int i, int n){
+    if(i > n) return;
+    cout << i << " ";
+    f2(1+i,n);
+}
 
+//Print from N to 1
+void f3(int i, int n){
+    if(i<1) return;
+    cout << i << " ";
+    f3(i-1,n);
+    //has to be f(n,n) on main
+}
 int main(){
     int n;
     cin >> n;
-    f(1,n);
+    f3(n,n);
 }
