5425726e66d7b7869f408f88c34b561410baa469
RayanGomes <magirayan84@gmail.com>
2025-05-09 12:37:50 +0600

    Bubble sort

--- 1.Sort(Selection,bubble,insertion).cpp
+++ 1.Sort(Selection,bubble,insertion).cpp
@@ -22,10 +22,28 @@ void selection_sort(int arr[], int n){
                 arr[min] = arr[i];
                 arr[i] = temp;
     }
-    
+     //Worst/Avg/best == 0(N^2)
 }
 
-
+void bubble_sort(int arr[], int n){
+    for (int i = n-1; i > 1; i--)
+    {
+        int didSwap = 0;
+        for (int j = 0; j < i - 1; j++)
+        {
+            if(arr[j] > arr[j+1]){
+                //Swap
+                int temp = arr[j+1];
+                arr[j+1] = arr[j];
+                arr[j] = temp;
+                didSwap = 1;
+            }
+        }
+        if(didSwap == 0) break; //Optimized Because of already sorted asc order.
+    }
+    //Best TC = O(N) --> already sorted order
+    //Worst/Avg == 0(N^2)
+}
 
 int main(){
 int n;
@@ -35,7 +53,7 @@ for (int i = 0; i < n; i++)
 {
     cin >> arr[i];
 }
-selection_sort(arr,n);
+bubble_sort(arr,n);
 for (int i = 0; i < n; i++)
 {
     cout << arr[i] << " ";
